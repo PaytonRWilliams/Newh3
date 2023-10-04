884d0f593214df1fcf157d608606b23c23aefaa6
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-04 09:58:20 -0500

    Update book_author-by-book.php

--- book_author-by-book.php
+++ book_author-by-book.php
@@ -7,7 +7,7 @@ require_once("model-book_author-by-book.php");
 
 $pageTitle="Book Author by Book";
 include "view-header.php";
-$book_authors = selectBook_AuthorsByBook($_POST['baid']);
+$book_authors = selectBook_AuthorsByBook($_POST['bid']);
 include "view-book_author-by-book.php";
 include "view-footer.php";
 ?>
