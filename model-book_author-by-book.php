1e7be802fd00a92079c83c1556d0d3eea21ec185
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-04 09:18:59 -0500

    Update and rename model-positions-by-book.php to model-book_author-by-book.php

--- model-book_author-by-book.php
+++ model-book_author-by-book.php
@@ -1,10 +1,10 @@
 <?php
-function selectPositionsByCourse($bid) {
+function selectPositionsByCourse($baid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT  b.book_id, book_title, book_genre, position
 FROM `book` b join book_author ba ON ba.book_id=b.book_id WHERE ba.book_id=? ");
-        $stmt->bind_param("i", $bid);
+        $stmt->bind_param("i", $baid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
