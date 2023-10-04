6c73387795f696a5d02430b5f66a3ea83c607604
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-04 09:05:07 -0500

    Create model-positions-by-book.php

---
+++ model-positions-by-book.php
@@ -0,0 +1,18 @@
+<?php
+function selectPositionsByCourse($bid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT  b.book_id, book_title, book_genre, position
+FROM `book` b join book_author ba ON ba.book_id=b.book_id WHERE ba.book_id=? ");
+        $stmt->bind_param("i", $bid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
