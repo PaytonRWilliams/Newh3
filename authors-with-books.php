55605d63f21d85dcb2d22a00997d3823165938b8
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-04 10:17:50 -0500

    Create authors-with-books.php

---
+++ authors-with-books.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("util-db.php");
+require_once("model-authors-with-books.php");
+
+
+
+$pageTitle="Authors With Books";
+include "view-header.php";
+$authors = selectAuthors();
+include "view-authors-with-books.php";
+include "view-footer.php";
+?>
