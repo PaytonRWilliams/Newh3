b131ce0ed26d52ebdfb8b0165144c8c7eefc1977
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-04 09:39:04 -0500

    Update view-books.php

--- view-books.php
+++ view-books.php
@@ -6,6 +6,7 @@
            <th>ID</th>
           <th>Title</th>
           <th>Genre</th>
+        <th>       </th>
       </tr>
        
 
@@ -21,7 +22,13 @@ while ($book= $books->fetch_assoc()) {
     
     <td><?php echo $book['book_title']; ?></td>
     <td><?php echo $book['book_genre']; ?></td>
+   <td>      
+           <form method = "post" action="sections-by-course.php">
+  <input type ="hidden" name="bid" value=" <?php echo $book['book_id']; ?>">
+  <button type="submit" class="btn btn-primary">Book_Authors</button>
+</form>
    
+   </td>
    
  </tr> 
 
