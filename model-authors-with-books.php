c8624ad41e14a8ff9da5a0bc75e599d7abe83acf
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-04 10:23:16 -0500

    Update model-authors-with-books.php

--- model-authors-with-books.php
+++ model-authors-with-books.php
@@ -12,5 +12,19 @@ function selectAuthors() {
         throw $e;
     }
 }
-
+function selectBooksByAuthor($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT  b.book_id, book_title, book_genre, position
+FROM `book` b join book_author ba ON ba.book_id=b.book_id WHERE ba.author_id=? ");
+        $stmt->bind_param("i", $aid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
