5831fe6e5c760ddaa9087d92c84a22d093f5418b
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-04 10:03:44 -0500

    Update model-book_author-by-book.php

--- model-book_author-by-book.php
+++ model-book_author-by-book.php
@@ -1,10 +1,10 @@
 <?php
-function selectBook_AuthorByBook($baid) {
+function selectBook_AuthorsByBook($bid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT  b.book_id, book_title, book_genre, position
 FROM `book` b join book_author ba ON ba.book_id=b.book_id WHERE ba.book_id=? ");
-        $stmt->bind_param("i", $baid);
+        $stmt->bind_param("i", $bid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
