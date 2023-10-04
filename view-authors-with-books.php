3515dde2780a21ae911cdb98443633bc0a6e7ef5
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-04 10:52:29 -0500

    Create view-authors-with-books.php

---
+++ view-authors-with-books.php
@@ -0,0 +1,47 @@
+<h1>Authors with Books</h1>
+<div class="card-group">
+ 
+
+
+
+
+
+<?php
+while ($author= $authors->fetch_assoc()) {
+
+?>
+
+  <div class="card">
+ 
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $author['author_name']; ?></h5>
+      <p class="card-text">
+<?php
+   $books = selectBooksByAuthor($author['author_id']);
+  while ($book = $books->fetch_assoc()) {
+    ?>
+
+      <?php
+        
+  }
+    
+    ?>
+
+
+        
+      </p>
+      <p class="card-text"><small class="text-body-secondary">Gender: <?php echo $author['author_gender']; ?> </small></p>
+    </div>
+  </div>
+
+
+
+
+      
+<?php
+
+}
+
+
+?>
+</div>
