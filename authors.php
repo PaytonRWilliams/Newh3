1d86e18bb43a0f446ded88b1ab32ba2724576797
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-04 04:22:02 -0500

    Create authors.php

---
+++ authors.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("util-db.php");
+require_once("model-authors.php);
+
+
+
+$pageTitle="Authors";
+include "view-header.php";
+$authors = selectAuthors();
+include "view-authors.php";
+include "view-footer.php";
+?>
