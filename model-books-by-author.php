a636899bca599cd2144775f3927bab592c1b25b7
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-04 08:36:03 -0500

    Create model-books-by-author.php

---
+++ model-books-by-author.php
@@ -0,0 +1,18 @@
+<?php
+function selectBooksByAuthor($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT b.book_id, ba.book_author_id, position
+        FROM `book` b join book_author ba ON ba.book_id=b.book_id WHERE ba.author_id=? ");
+        $stmt->bind_param("i", $aid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
