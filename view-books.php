e630609e1b2e51b68a3a7a8ae7788ce849e2afc4
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-24 09:02:25 -0500

    Update view-books.php

--- view-books.php
+++ view-books.php
@@ -50,7 +50,7 @@ while ($book= $books->fetch_assoc()) {
    <td>
      <form method = "post" action="">
   <input type ="hidden" name="bid" value=" <?php echo $book['book_id']; ?>">
-     <input type ="hidden" name="actionType" value="Delete ">
+     <input type ="hidden" name="actionType" value="Delete">
   <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
