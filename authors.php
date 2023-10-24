47fb2c97e9bb1b0fe7d6943c130c0b5ab8f3f94f
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-24 10:49:15 -0500

    Update authors.php

--- authors.php
+++ authors.php
@@ -7,7 +7,58 @@ require_once("model-authors.php");
 
 $pageTitle="Authors";
 include "view-header.php";
+
+
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']){
+
+  case "Add":
+  if (insertAuthors(            ) {
+      echo '<div class="alert alert-success" role="alert">Author added.</div>';
+  } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+  }
+  break;
+
+ case "Delete":
+  if (deleteAuthors($_POST['          '])) {
+      echo '<div class="alert alert-success" role="alert">Author deleted.</div>';
+  } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+  }
+  break;
+
+        
+
+  }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 $authors = selectAuthors();
 include "view-authors.php";
 include "view-footer.php";
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
