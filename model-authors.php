57e37ba15795059113f9fead657342a26100ba25
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-24 11:01:21 -0500

    Update model-authors.php

--- model-authors.php
+++ model-authors.php
@@ -13,6 +13,79 @@ function selectAuthors() {
     }
 }
 
+
+
+function insertAuthors($aName, $aGender) {
+   try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `author` (`author_name`, `author_gender`) VALUES (?, ?)");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
 
 
+
+
+
+
+function insertBooks($bTitle, $bGenre, $pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `book` (`book_title`, `book_genre`, `publisher_id`) VALUES (?, ?, ?)");
+        $stmt->bind_param("ssi", $bTitle, $bGenre, $pid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateBooks($bTitle, $bGenre, $bid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update 'book' set 'book_title'=?, 'book_genre'= ? where book_id = ? ");
+        $stmt->bind_param("ssi", $bTitle, $bGenre, $bid);
+       $success= $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteBooks($bid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from book where book_id=?");
+        $stmt->bind_param("i", $bid);
+       $success= $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+
+
+
+
+
+
+
+?>
