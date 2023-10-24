1c47534c8b889ab68708e96c30f63ebbc91a0a2d
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-23 21:29:30 -0500

    Update books.php

--- books.php
+++ books.php
@@ -7,6 +7,19 @@ require_once("model-books.php");
 
 $pageTitle="Books";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']){
+
+  case "Add";
+  insertBook($_POST['bTitle'], $_POST['bGenre']);
+  break;
+
+  }
+  }
+
+
+
 $books = selectBooks();
 include "view-books.php";
 include "view-footer.php";
