1f7f214a68e7289d92444dc6688d7637e33bc850
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-25 10:20:29 -0500

    Update publishers.php

--- publishers.php
+++ publishers.php
@@ -7,7 +7,56 @@ require_once("model-publishers.php");
 
 $pageTitle="Publishers";
 include "view-header.php";
-$publishers = selectPublishers();
+
+
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']){
+
+  case "Add":
+  if (insertPublishers($_POST['pName'], $_POST['pCity'])) {
+      echo '<div class="alert alert-success" role="alert">Publisher added.</div>';
+  } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+  }
+  break;
+
+case "Edit":
+  if (updatePublishers($_POST['pName'], $_POST['pCity'], $_POST['pid'])) {
+      echo '<div class="alert alert-success" role="alert">Publisher edited.</div>';
+  } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+  }
+  break;
+
+
+
+
+        
+ case "Delete":
+  if (deleteAuthors($_POST['pid'])) {
+      echo '<div class="alert alert-success" role="alert">Publisher deleted.</div>';
+  } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+  }
+  break;
+        
+
+  }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+$authors = selectPublishers();
 include "view-publishers.php";
 include "view-footer.php";
 ?>
