ffe821c9239573cc78424ac8fbc3edbf22cb9356
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-25 10:06:22 -0500

    Update publishers.php

--- publishers.php
+++ publishers.php
@@ -7,7 +7,56 @@ require_once("model-publishers.php");
 
 $pageTitle="Publishers";
 include "view-header.php";
-$publishers = selectPublishers();
-include "view-publishers.php";
+
+
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']){
+
+  case "Add":
+  if (insertPublishers($_POST['pName'], $_POST['pcity'])) {
+      echo '<div class="alert alert-success" role="alert">Author added.</div>';
+  } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+  }
+  break;
+
+case "Edit":
+  if (updateAuthors($_POST['aName'], $_POST['aGender'], $_POST['aid'])) {
+      echo '<div class="alert alert-success" role="alert">Author edited.</div>';
+  } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+  }
+  break;
+
+
+
+
+        
+ case "Delete":
+  if (deleteAuthors($_POST['aid'])) {
+      echo '<div class="alert alert-success" role="alert">Author deleted.</div>';
+  } else {
+      echo '<div class="alert alert-danger" role="alert">Error.</div>';
+  }
+  break;
+        
+
+  }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+$authors = selectAuthors();
+include "view-authors.php";
 include "view-footer.php";
 ?>
