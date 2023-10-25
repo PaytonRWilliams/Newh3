b3d708b7f55ce355130efaff1161101e1f66b3fc
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-25 07:44:44 -0500

    Update model-books-by-author.php

--- model-books-by-author.php
+++ model-books-by-author.php
@@ -1,4 +1,20 @@
+
+
 <?php
+function selectAuthors() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT author_id, author_name FROM `author`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 function selectBooksByAuthor($aid) {
     try {
         $conn = get_db_connection();
@@ -15,4 +31,73 @@ FROM `book` b join book_author ba ON ba.book_id=b.book_id WHERE ba.author_id=? "
     }
 }
 
+function selectInstructorsForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT author_id, author_name FROM `author` order by author_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectBooksForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT book_id, book_title FROM `book` order by book_title");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertBook_Author($aid, $bid, $position) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `book_author` (`author_id`, `book_id`, `position`) VALUES (?, ?, ?)");
+        $stmt->bind_param("iis", $aid, $bid, $position;
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateBook_Author($aid, $bid, $position, $baid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `book_author` set `author_id` = ?, `book_id` = ?, `position` = ? `where book_author_id = ?");
+        $stmt->bind_param("iisi", $aid, $bid, $position, $baid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteBook_Author($baid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from book_author where book_author_id=?");
+        $stmt->bind_param("i", $baid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
