4c2890871289dda60c6d69a9ad5fe66aaa248e96
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-25 10:41:48 -0500

    Update view-authors-editform.php

--- view-authors-editform.php
+++ view-authors-editform.php
@@ -1,5 +1,5 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editBookModal<?php echo $book['book_id']; ?>">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editAuthorModal<?php echo $author['author_id']; ?>">
 
 
 
