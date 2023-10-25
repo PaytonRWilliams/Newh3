f383575ad797be5e1a2e122774d7f906cfc2eda7
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-25 08:19:29 -0500

    Update books-by-author.php

--- books-by-author.php
+++ books-by-author.php
@@ -1,37 +1,13 @@
 <?php
+
 require_once("util-db.php");
-require_once("model-books-by-authors.php");
+require_once("model-books-by-author.php");
 
-$pageTitle = "Books by Authors";
+
+
+$pageTitle="Books by Authorr";
 include "view-header.php";
-
-if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
-    case "Add":
-      if (insertBook_Author($_POST['aid'], $_POST['bid'], $_POST['position'])) {
-        echo '<div class="alert alert-success" role="alert">Section added.</div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-    case "Edit":
-      if (updateBook_Author($_POST['aid'], $_POST['bid'], $_POST['position'], $_POST['baid'])) {
-        echo '<div class="alert alert-success" role="alert">Section edited.</div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-    case "Delete":
-      if (deleteBook_Author($_POST['baid'])) {
-        echo '<div class="alert alert-success" role="alert">Section deleted.</div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-  }
-}
-
-$authors = selectAuthors();
+$books = selectBooksByAuthor($_GET['id']);
 include "view-books-by-author.php";
 include "view-footer.php";
 ?>
