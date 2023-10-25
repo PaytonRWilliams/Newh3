e37463c6ed8ecf09e1c6dbc11ba5bf02ab9d9379
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-25 09:00:42 -0500

    Update view-authors-with-books-editform.php

--- view-authors-with-books-editform.php
+++ view-authors-with-books-editform.php
@@ -1,5 +1,5 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editSectionModal<?php echo $course['section_id']; ?>">
+<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editBookAuthorModal<?php echo $book['book_id']; ?>">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
       <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
       <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -7,40 +7,33 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="editSectionModal<?php echo $course['section_id']; ?>" tabindex="-1" aria-labelledby="editSectionModalLabel<?php echo $course['section_id']; ?>" aria-hidden="true">
+<div class="modal fade" id="editBookAuthorModal<?php echo $book['book_author_id']; ?>" tabindex="-1" aria-labelledby="editBookAuthorModalLabel<?php echo $book['book_author_id']; ?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editSectionModalLabel<?php echo $course['section_id']; ?>">Edit section</h1>
+        <h1 class="modal-title fs-5" id="editSectionModalLabel<?php echo $book['book_author_id']; ?>">Edit section</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form method="post" action="">
           <div class="mb-3">
-            <label for="iid<?php echo $course['section_id']; ?>" class="form-label">Instructor</label>
+            <label for="aid<?php echo $book['book_author_id']; ?>" class="form-label">Author</label>
 <?php
-$instructorList = selectInstructorsForInput();
-$selectedInstructor = $course['instructor_id'];
-include "view-instructor-input-list.php";
+$authorList = selectAuthorsForInput();
+$selectedAuthor = $book['author_id'];
+include "view-author-input-list.php";
 ?>
           </div>
           <div class="mb-3">
-            <label for="cid<?php echo $course['section_id']; ?>" class="form-label">Course</label>
-            <input type="text" class="form-control" id="cid<?php echo $course['section_id']; ?>" name="cid" value="<?php echo $course['course_id']; ?>">
+            <label for="bid<?php echo $book['book_author_id']; ?>" class="form-label">Book</label>
+            <input type="text" class="form-control" id="bid<?php echo $book['book_author_id']; ?>" name="bid" value="<?php echo $book['book_id']; ?>">
           </div>
           <div class="mb-3">
-            <label for="sem<?php echo $course['section_id']; ?>" class="form-label">Semester</label>
-            <input type="text" class="form-control" id="sem<?php echo $course['section_id']; ?>" name="sem" value="<?php echo $course['semester']; ?>">
+            <label for="position<?php echo $book['book_author_id']; ?>" class="form-label">Position</label>
+            <input type="text" class="form-control" id="positon<?php echo $course['section_id']; ?>" name="position" value="<?php echo $book['position']; ?>">
           </div>
-          <div class="mb-3">
-            <label for="room<?php echo $course['section_id']; ?>" class="form-label">Room</label>
-            <input type="text" class="form-control" id="room<?php echo $course['section_id']; ?>" name="room" value="<?php echo $course['room']; ?>">
-          </div>
-          <div class="mb-3">
-            <label for="daytime<?php echo $course['section_id']; ?>" class="form-label">Day/ time</label>
-            <input type="text" class="form-control" id="daytime<?php echo $course['section_id']; ?>" name="daytime" value="<?php echo $course['day_time']; ?>">
-          </div>
-            <input type="hidden" name="sid" value="<?php echo $course['section_id']; ?>">
+        
+            <input type="hidden" name="sid" value="<?php echo $book['book_author_id']; ?>">
             <input type="hidden" name="actionType" value="Edit">
           <button type="submit" class="btn btn-primary">Save</button>
         </form>
