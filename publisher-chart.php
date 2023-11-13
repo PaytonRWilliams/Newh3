5ebdb0e48345a95c6857945a10cca1835a0b30a1
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-11-13 16:35:03 -0600

    Create publisher-chart.php

---
+++ publisher-chart.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once("util-db.php");
+
+
+
+
+$pageTitle="publisher chart";
+include "view-header.php";
+include "view-publisher-chart.php";
+include "view-footer.php";
+
+
+
+
+
+?>
