76ea5dca273b47520cf63bc09048b4b28d436b8b
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-11-13 16:41:38 -0600

    Update view-publisher-chart.php

--- view-publisher-chart.php
+++ view-publisher-chart.php
@@ -1,22 +1,35 @@
-<h1>Book chart</h1>
+<h1>Publisher chart</h1>
 
 
 
 
-<head>
-  <!-- Plotly.js -->
-  <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-</head>
+ <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {
+        'packages':['geochart'],
+      });
+      google.charts.setOnLoadCallback(drawRegionsMap);
 
-<div id="tester" style="width:600px;height:250px;"></div>
-<!-- Plots go in blank <div> elements. 
-    You can size them in the plot layout,
-    or give the div a size as shown here.
--->
-<script>
-	TESTER = document.getElementById('tester');
-	Plotly.newPlot( TESTER, [{
-	x: [1, 2, 3, 4, 5],
-	y: [1, 2, 4, 8, 16] }], {
-	margin: { t: 0 } } );
-</script>
+      function drawRegionsMap() {
+        var data = google.visualization.arrayToDataTable([
+          ['Country', 'Popularity'],
+          ['Germany', 200],
+          ['United States', 300],
+          ['Brazil', 400],
+          ['Canada', 500],
+          ['France', 600],
+          ['RU', 700]
+        ]);
+
+        var options = {};
+
+        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="regions_div" style="width: 900px; height: 500px;"></div>
+  </body>
