84b57926efca2879d88166b8b88ba14cdeb4ea6c
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-11-13 14:36:02 -0600

    Update authors-chart.php

--- authors-chart.php
+++ authors-chart.php
@@ -1,13 +1,12 @@
 <?php
 
 require_once("util-db.php");
-require_once("model/authors-chart-db.php");
+
 
 
 
 $pageTitle="Authors chart";
 include "view-header.php";
-$authors = selectAuthors();
-include "view/authors-chart/page.php";
+
 include "view-footer.php";
 ?>
