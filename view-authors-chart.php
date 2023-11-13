5a5e4bfcb368275d22e397692f5fa4fe3f06226e
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-11-13 15:41:05 -0600

    Update view-authors-chart.php

--- view-authors-chart.php
+++ view-authors-chart.php
@@ -1,25 +1,29 @@
 <h1>Authors chart</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<?php
+
 <script>
   const ctx = document.getElementById('myChart');
 
   new Chart(ctx, {
-    type: 'bar',
+    type: 'doughnut',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
+    datasets: [{
+        data: [10, 20, 30]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Red',
+        'Yellow',
+        'Blue'
+    ]
+},
+   
   });
 </script>
